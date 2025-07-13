b7be176b51a30933c1571cada07f91b4dd27afe2
nima-nkz <nimanikrouz@gmail.com>
2025-07-13 18:44:14 +0330

    data fetcher: driver data insertion.

--- backend/src/data-fetcher.ts
+++ backend/src/data-fetcher.ts
@@ -1,5 +1,5 @@
 import axios, {AxiosError, AxiosResponse, AxiosRequestConfig} from "axios";
-import {PrismaClient} from '@prisma/client';
+import {PrismaClient} from './../generated/prisma/client'
 
 interface Session {
 
@@ -18,7 +18,7 @@ interface Session {
     gmt_offset: string,
     year: number
 }
-interface Driver   {
+interface Driver_interface   {
     meeting_key: number,
     session_key: number,
     driver_number: number,
@@ -32,9 +32,7 @@ interface Driver   {
     headshot_url: string,
     country_code: string
 }
-interface CostumReject {
-    header: string,
-}
+
 
 
 class  DataFetcher{
@@ -51,9 +49,9 @@ class  DataFetcher{
         }
     }
 
-    async fetchDrivers(meetingKey:number): Promise<Driver[]>{
+    async fetchDrivers(meetingKey:number): Promise<Driver_interface[]>{
         try {
-        const response :AxiosResponse<Driver[],AxiosRequestConfig> = await axios.get<Driver[]>(`${this.baseURL}/drivers?meeting_key=${meetingKey.toString()}`);
+        const response :AxiosResponse<Driver_interface[],AxiosRequestConfig> = await axios.get<Driver_interface[]>(`${this.baseURL}/drivers?meeting_key=${meetingKey.toString()}`);
         // console.log(response.data);
         return response.data;
         }catch(error) {
@@ -61,8 +59,60 @@ class  DataFetcher{
             throw new Error(`failed to fetch drivers: ${axiosError.message}`);
         }
     }
+};
+
+
+class DataInserter {
+    private prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+
+    async insertDrivers(drivers_list: Driver_interface[]): Promise<void> {
+        try {
+            if (!drivers_list.length) {
+                throw new Error("No drivers found.");
+            }
+            else{
+                await this.prisma.driver.createMany({
+                    data :drivers_list.map((driver_member:Driver_interface) => ({
+                        id : driver_member.broadcast_name,
+                        name : driver_member.full_name,
+                        driver_number : driver_member.driver_number,
+                        team_id: driver_member.team_name,
+                        nationality: driver_member.country_code
+                    })),
+                    skipDuplicates:true,
+                });
+                console.log(`driver was inserted: ${drivers_list.length}`);
+            }
+
+        }catch (error) {
+            console.error(`failed to insert drivers: ${error}`);
+        }
+    }
+
+
+    async disconnect(): Promise<void> {
+        await this.prisma.$disconnect();
+
+    };
 }
 
-const mydataFetcher = new DataFetcher();
-mydataFetcher.fetchSessions(2023);
-mydataFetcher.fetchDrivers(1224);
+
+async function main(){
+    const mydataFetcher = new DataFetcher();
+    const myInserter = new DataInserter();
+    for (const year of [2023,2024]){
+        const sessions : Session[] = await mydataFetcher.fetchSessions(year)
+        const meetingKeys : number[] = [...new Set(sessions.map(x=>x.meeting_key))];
+        for (const meetingKey of meetingKeys){
+            const drivers:Driver_interface[] = await mydataFetcher.fetchDrivers(meetingKey);
+            await myInserter.insertDrivers(drivers);
+        }
+    }
+    myInserter.disconnect();
+}
+
+main().catch(error => console.error(`main() failed: ${error}`));
